4a29217bee36f09c5709dca2c1fa616b6a82bb3f
monvas <monvas-3@student.ltu.se>
2023-11-20 16:12:17 +0100

    updated structure for new ui

--- main.py
+++ main.py
@@ -1,72 +1,32 @@
-import os
-from s3.operations import S3Operations
-from s3.list_buckets import ListBuckets
-from s3.create_buckets import CreateBuckets
+from ui.menu import ModernUI
+import tkinter as tk
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    ui_app = ModernUI(root)
+    ui_app.root.mainloop()
+
+# from cloudwatch.ec2metrics import CloudwatchMetrics
 # import tkinter as tk
-# from tkinter import messagebox
+# from tkinter import ttk
+# from ttkthemes import ThemedTk
+# from PIL import Image, ImageTk
 
-def run():
-    print("The following actions are available: 1. Create S3 buckets, 2. List S3 buckets, 3. Operations in S3")
-    action = input("Enter the number of the action you want to perform: ")
+# # def main():
+# #     # Replace 'YOUR_INSTANCE_ID' with your actual EC2 instance ID
+# #     instance_id = 'i-0d01fafb039e0ad94'
 
-    # Create S3 buckets
-    if action == "1":
-        create_bucket()
+# #     # List of metrics to monitor
+# #     metrics_to_monitor = ['CPUUtilization', 'DiskReadBytes', 'DiskWriteBytes', 'NetworkIn', 'NetworkOut']
 
-    # List S3 buckets
-    if action == "2":
-        list_buckets()
+# #     # Instantiate the class
+# #     ec2_metrics = CloudwatchMetrics(region='eu-north-1')
 
-    # Operations in S3
-    if action == "3":
-        operations_in_s3()
-     
+# #     for metric in metrics_to_monitor:
+# #         data_points = ec2_metrics.get_ec2_metrics(instance_id, metric, period=300)
+# #         if data_points:
+# #             print(f"{metric} Data Points: {data_points}")
+# #         else:
+# #             print(f"No data available for {metric}")
 
-def create_bucket():
-    num_buckets = input("How many buckets do you want to create?: ")
-    for num in num_buckets:
-        bucket_name = input("Enter a bucket name: ")
-        bucket_region = input("Enter a region: ")
-        # Instantiate CreateBuckets class
-        s3_bucket_creation = CreateBuckets(bucket_name,bucket_region)
-        s3_bucket_creation.create_bucket()  
 
-def list_buckets():
-    # Get the region from the user 
-    region = input("Enter region to list the existing buckets in that region: ")
-    # Instantiate ListBuckets class
-    s3_bucket_list = ListBuckets(region)
-    # Output the bucket names in the specified region
-    print("The list of existing buckets in the " + region + " are:")
-    for bucket in s3_bucket_list.get_buckets_in_region():
-        print(bucket)
-
-def operations_in_s3():
-   # Operations in S3
-    print("Operations Available: 1. Upload, 2. Download and 3. Delete objects in S3")
-    operation = input("Enter the number of the operation you want to perform: ")
-    bucket_name = input("Enter the bucket name: ")
-    region_operation = input("Enter the region: ")
-
-    # Instance of S3Operations class
-    s3_operations = S3Operations(region_operation)
-
-    # Upload objects to S3 bucket
-    if operation == "1":
-        file_path = input("Enter the file path: ")
-        object_key = input("Enter the object key: ")
-        s3_operations.upload_s3_object(file_path, bucket_name, object_key)
-
-    # Download objects from S3 bucket
-    if operation == "2":
-        object_name = input("Enter the name of the file to be downloaded: ")
-        object_key = input("Enter the object key: ")
-        s3_operations.download_s3_object(bucket_name, object_key, object_name) 
-
-    # Delete objects from S3 bucket
-    # if operation == "3":
-    #     object_key = input("Enter the object key: ")
-    #     DeleteS3Object(bucket_name, object_key)
-
-if __name__ == '__main__':
-    run()
