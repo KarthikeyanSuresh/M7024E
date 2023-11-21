6e7321c418ad1bd1075b97da325cbc2787d7126d
monvas <monvas-3@student.ltu.se>
2023-11-21 14:01:49 +0100

    Added S3 Operations

--- ui/menu.py
+++ ui/menu.py
@@ -1,8 +1,14 @@
 import tkinter as tk
 from tkinter import ttk
+from tkinter import messagebox
+import tkinter.simpledialog as sd 
+import tkinter.filedialog as filedialog
 from PIL import Image, ImageTk
 from ui.ec2 import EC2Operations
 from cloudwatch.ec2metrics import CloudwatchMetrics
+from s3.operations import S3Operations
+from s3.list_buckets import ListBuckets
+from s3.create_buckets import CreateBuckets
 
 class ModernUI:
     def __init__(self, root):
@@ -59,6 +65,24 @@ class ModernUI:
 
         return_button = ttk.Button(self.root, text="Return to Main Menu", command=self.create_main_menu)
         return_button.pack(pady=20)
+    
+    def show_s3_menu(self):
+        self.clear_frame()
+        ttk.Label(self.root, text="S3 Functions", font=('Helvetica', 16)).pack(pady=20)
+        self.label = tk.Label(self.root, text="The following actions are available: 1. Create S3 buckets, 2. List S3 buckets, 3. Operations in S3. Choose an action:", font=("Consolas", 12))
+        self.label.pack(pady=10, padx=50)
+
+        self.create_button = tk.Button(self.root, text="Create S3 Buckets", command=self.create_buckets, font=("Consolas", 12))
+        self.create_button.pack(padx=10, pady=10)
+
+        self.list_button = tk.Button(self.root, text="List S3 Buckets", command=self.list_buckets, font=("Consolas", 12))
+        self.list_button.pack(padx=10, pady=10)
+
+        self.operations_button = tk.Button(self.root, text="Operations in S3", command=self.operations_in_s3, font=("Consolas", 12))
+        self.operations_button.pack(padx=10, pady=10)
+        return_button = ttk.Button(self.root, text="Return to Main Menu", command=self.create_main_menu)
+        return_button.pack(pady=20)
+
 
     def show_ec2_metrics_ui(self):
         self.clear_frame()
@@ -114,13 +138,42 @@ class ModernUI:
         # Implement code to show EC2 instance status here
         ttk.Label(self.root, text="Showing EC2 instance status...").pack(pady=20)
 
-    def show_s3_menu(self):
-        self.clear_frame()
+    def create_buckets(self):
+        num_buckets = int(sd.askstring("Input", "How many buckets do you want to create?"))
+        for i in range(num_buckets):
+            bucket_name = sd.askstring("Input", "Enter a bucket name:")
+            bucket_region = sd.askstring("Input", "Enter a region:")
+            s3_bucket_creation = CreateBuckets(bucket_name, bucket_region)
+            s3_bucket_creation.create_bucket()
 
-        ttk.Label(self.root, text="S3 Functions", font=('Helvetica', 16)).pack(pady=20)
+    def list_buckets(self):
+        region = sd.askstring("Input", "Enter region to list the existing buckets in that region:")
+        s3_bucket_list = ListBuckets(region)
+        buckets = s3_bucket_list.get_buckets_in_region()
+        message = f"The list of existing buckets in the {region} region are:\n{', '.join(buckets)}"
+        messagebox.showinfo("List of Buckets", message)
 
-        # Replace the following line with the specific S3 functions UI
-        ttk.Label(self.root, text="S3 functions go here...").pack(pady=20)
+    def operations_in_s3(self):
+        operation = sd.askstring("Input", "Enter the number of the operation you want to perform:\n1. Upload\n2. Download\n3. Delete")
+        bucket_name = sd.askstring("Input", "Enter the bucket name:")
+        region_operation = sd.askstring("Input", "Enter the region:")
+
+        if operation == "1":
+            file_path = filedialog.askopenfilename(title="Select file to upload")
+            s3_operations = S3Operations(region_operation)
+            object_key = sd.askstring("Input", "Enter the object key:")
+            s3_operations.upload_s3_object(file_path, bucket_name, object_key)
+
+        elif operation == "2":
+            object_name = sd.askstring("Input", "Enter the name of the file to be downloaded:")
+            s3_operations = S3Operations(region_operation)
+            object_key = sd.askstring("Input", "Enter the object key:")
+            s3_operations.download_s3_object(bucket_name, object_key, object_name)
+
+        elif operation == "3":
+            object_key = sd.askstring("Input", "Enter the object key:")
+            s3_operations = S3Operations(region_operation)
+            s3_operations.delete_s3_object(bucket_name, object_key)
 
         return_button = ttk.Button(self.root, text="Return to Main Menu", command=self.create_main_menu)
         return_button.pack(pady=20)
