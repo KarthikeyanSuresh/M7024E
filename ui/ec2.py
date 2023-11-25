0dd13eacbe7128fe707ea4cbf0cedc4f5a19005d
monvas <monvas-3@student.ltu.se>
2023-11-25 20:40:32 +0100

    clean code

--- ui/ec2.py
+++ ui/ec2.py
@@ -1,9 +1,9 @@
 from cloudwatch.ec2metrics import CloudwatchMetrics
 
 class EC2Operations:
-    def fetch_metrics(self, ec2_metrics, instance_id, metric_name):
+    def fetch_metrics(self, ec2_metrics, instance_id, metric_name, period):
         if instance_id and metric_name:
-            data_points = ec2_metrics.get_ec2_metrics(instance_id, metric_name)
+            data_points = ec2_metrics.get_ec2_metrics(instance_id, metric_name, period)
             return data_points
         else:
             return None
